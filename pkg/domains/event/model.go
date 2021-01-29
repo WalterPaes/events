a7b3a45040640dac5d949fb01b3a3eaa216e2194
Walter Junior <walter.junior@endurance.com>
2021-01-29 19:42:24 -0300

    Create Event's methods

--- pkg/domains/event/model.go
+++ pkg/domains/event/model.go
@@ -1,5 +1,10 @@
 package event
 
+import (
+	"errors"
+	"events/pkg/domains/account"
+)
+
 type EventType string
 
 const (
@@ -13,4 +18,25 @@ type Event struct {
 	Amount      float64   `json:"amount"`
 	Destination int       `json:"destination"`
 	Origin      int       `json:"origin"`
+}
+
+func (e *Event) Handler() error {
+	var err error
+
+	switch e.Type {
+	case deposit:
+		acc := account.GetAccount(e.Destination)
+		err = acc.Deposit(e.Amount)
+	case withdraw:
+		acc := account.GetAccount(e.Origin)
+		err = acc.Withdraw(e.Amount)
+	case transfer:
+		accOr := account.GetAccount(e.Origin)
+		accDest := account.GetAccount(e.Destination)
+		err = accOr.Transfer(e.Amount, accDest.GetId())
+	default:
+		err = errors.New("invalid event")
+	}
+
+	return err
 }
