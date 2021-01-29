4d1dc1e883ec9f3393c29cecbe997dadd8d63bbe
Walter Junior <walter.junior@endurance.com>
2021-01-29 19:13:59 -0300

    Create account's methods based on event's types

--- pkg/domains/account/model.go
+++ pkg/domains/account/model.go
@@ -1,5 +1,12 @@
 package account
 
+import "errors"
+
+var (
+	InvalidValueOfAmountError = errors.New("invalid value of amount")
+	InsufficientFundsError = errors.New("insufficient funds")
+)
+
 type Account struct {
 	id int
 	balance float64
@@ -19,4 +26,40 @@ func (a *Account) GetId() int {
 
 func (a *Account) GetBalance() float64 {
 	return a.balance
+}
+
+func (a *Account) Deposit(amount float64) error {
+	if amount < 0 {
+		return InvalidValueOfAmountError
+	}
+	a.balance += amount
+	return nil
+}
+
+func (a *Account) Withdraw(amount float64) error {
+	if a.balance < amount {
+		return InsufficientFundsError
+	}
+	a.balance -= amount
+	return nil
+}
+
+func (a *Account) Transfer(amount float64, destinationId int) error {
+	if a.balance < amount {
+		return InsufficientFundsError
+	}
+
+	destination := GetAccount(destinationId)
+
+	err := a.Withdraw(amount)
+	if err != nil {
+		return err
+	}
+
+	err = destination.Deposit(amount)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
